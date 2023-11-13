839d558f797f758ebfd4f7fbba764bfddf187be3
mk <mk@mks-air-2.lan>
2023-11-12 20:40:40 -0500

    Updates: v1.2

--- cli.js
+++ cli.js
@@ -2,10 +2,11 @@
 
 var figlet = require('figlet');
 var clear = require('clear');
-const { llog } = require('./src/ll-modules/ll-utilities')
-const { transcribeFile } = require('./src/ll-bots/transcription-bot')
-const hackmd2pdf = require('./src/ll-modules/ll-hackmd-tools/hackmd-to-pdf')
-const { finetune } = require('./src/ll-bots/open-ai-bot')
+const { llog } = require('./src/utils/ll-utilities')
+// const { transcribeFile } = require('./src/ll-bots/transcription-bot')
+// const hackmd2pdf = require('./src/utils/ll-hackmd-tools/hackmd-to-pdf')
+// const { finetune } = require('./src/ll-bots/open-ai-bot')
+const { logMidi } = require('./src/utils/ll-midi-tools')
 require("dotenv").config({ path: __dirname + `/.env.cli` });
 
 
@@ -16,16 +17,21 @@ console.log("launching it.")
 
 // options: rename, makefolders, proxy, proxyf2, 
 
-if (yargs.transcribe) {
-    transcribeFile({filePath: yargs.transcribe})
-} else if (yargs.rename) {
-    llog.magenta(`going to rename`, yargs)
-} else if (yargs.phackmd) {
-    llog.magenta(`going to convert ${yargs.phackmd} to markdown`);
-    hackmd2pdf(yargs.phackmd);
-} else if (yargs.finetune) {
-    llog.magenta(`going to handle ${yargs.finetune}`);
-    finetune({txt: yargs.txt});
+// if (yargs.transcribe) {
+//     transcribeFile({filePath: yargs.transcribe})
+// } else if (yargs.rename) {
+//     llog.magenta(`going to rename`, yargs)
+// } else if (yargs.phackmd) {
+//     llog.magenta(`going to convert ${yargs.phackmd} to markdown`);
+//     hackmd2pdf(yargs.phackmd);
+// } else if (yargs.finetune) {
+//     llog.magenta(`going to handle ${yargs.finetune}`);
+//     finetune({txt: yargs.txt});
+// } else 
+
+if (yargs.logMidi) {
+    llog.magenta(`going to log midi input`);
+    logMidi();
 } else {
     console.log(`sorry, you didn't enter a recognized command.`)
 }
